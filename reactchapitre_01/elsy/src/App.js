9aa535ddf23d504bbd41dc823ebd4e7945b21739
mridha-emran <bari.lala490@gmail.com>
2021-07-19 17:26:28 +0200

    elsy2 ex06-ex08

--- reactchapitre_01/elsy/src/App.js
+++ reactchapitre_01/elsy/src/App.js
@@ -11,21 +11,83 @@ const heartMax =180
 const stepsMin = 0
 const stepsMax =50000
 
+
+
 class App extends React.Component {
+
+  constructor(){
+    super()
+    this.state={
+      water : 0,
+      heart : 120,
+      temperature : -10,
+      steps  : 3000
+    }
+    this.onHeartChange=this.onHeartChange.bind(this)
+    this.onStepsChange=this.onStepsChange.bind(this)
+    this.onTemperatureChange=this.onTemperatureChange.bind(this)
+  }
+
+  onHeartChange(e){
+   this.setState(
+      {
+        heart:e.target.value
+      }
+      )
+      this.calculateWater()
+    
+  }
+  onStepsChange(e){
+    this.setState(
+      {
+        steps:e.target.value
+      }
+    )
+    this.calculateWater()
+  }
+  onTemperatureChange(e){
+    this.setState(
+      {
+        temperature:e.target.value
+      }
+    )
+    this.calculateWater()
+  }
+
+  calculateWater(){
+    let addTemp=0
+    let addStaps=0
+    let addHeart=0
+    if (this.state.temperature > 20){
+      addTemp = (this.state.temperature - 20) * 0.02
+    }
+
+   else if (this.state.heart > 120) {
+    addHeart= (this.state.heart - 120) * 0.0008
+   } 
+   
+   else if (this.state.steps > 10000) {
+    addStaps = (this.state.steps - 10000) * 0.00002
+  }
+  const totel= addTemp+addStaps+addHeart
+  this.setState=({water:(1.5+totel).toFixed(2)})
+}
+  
   render() {
+   
     return (
       <div className="container-fluid">
 
         <div className="row">
 
-          <p> Heart: {heartMin}</p>
+          {/* <p> Heart: {heartMin}</p>
           <p> Temperature: {tempMin}</p>
-          <p> Steps: {stepsMin}</p>
+          <p> Steps: {stepsMin}</p> */}
 
-          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" > </Box>
-          <Box icon="directions_walk" color="black" value={3000} unit="steps" />
-          <Box icon="favorite" color="red" value={120} unit="bpm" />
-          <Box icon="wb_sunny" color="yellow" value={10} unit="°c" />
+          <Box icon="local_drink" color="#3A85FF"  value={1.5} unit="L" > </Box>
+          <Box icon="directions_walk" color="black" min={stepsMin} max={stepsMax} value={this.state.steps} unit="steps" onChangeSlide={this.onStepsChange}/>
+          <Box icon="favorite" color="red" min={heartMin} max={heartMax} value={this.state.heart} unit="bpm" onChangeSlide={this.onHeartChange} />
+          <Box icon="wb_sunny" color="yellow" min={tempMin} max={tempMax} value={this.state.temperature} unit="°c" onChangeSlide={this.onTemperatureChange} />
         
         </div>
       </div>
--- reactchapitre_01/elsy/src/components/Box.js
+++ reactchapitre_01/elsy/src/components/Box.js
@@ -4,6 +4,12 @@ import React from "react"
 
 class Box extends React.Component{
     render(){
+        const slider=()=>{
+            if(this.props.unit !=="L"){
+              return(<input  type ="range" min={this.props.min} max={this.props.max} value={this.props.value} onChange={this.props.onChangeSlide}></input>)
+      
+            }
+          }
         return(
             <div className="box col-sm-3 col-6">
             <span className="material-icons" style={
@@ -11,6 +17,7 @@ class Box extends React.Component{
                     {this.props.icon}
                 </span>
                 <p>{this.props.value} {this.props.unit}</p>
+                {slider()}
             </div>
         )
 
