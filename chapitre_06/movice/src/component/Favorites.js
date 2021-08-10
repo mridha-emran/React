34b28934d13206f12a2741100b3501df59bdc28c
mridha-emran <bari.lala490@gmail.com>
2021-08-10 17:31:29 +0200

    favoite done

--- chapitre_06/movice/src/component/Favorites.js
+++ chapitre_06/movice/src/component/Favorites.js
@@ -1,16 +1,66 @@
-import React from "react"
+  
+import React, { Component } from 'react'
+import Card from './Card'
 
-class Favorites extends React.Component{
+export class Favorites extends Component {
 
-    getStorage() {
+    constructor() {
+        super()
+
+        this.state = {
+            movies: [],
+            favIds: this.getStorage()
+        }
        
     }
-    render(){
-        return(
+
+    getStorage =()=> {
+        const idsFavorites = JSON.parse(localStorage.getItem("favorites")) || []
+        return idsFavorites
+    }
+
+
+
+    getMovie=(id)=> {
+        fetch( `http://api.themoviedb.org/3/movie/${id}?api_key=20e0cad7b9b54645e2cd9dc394f11db8`)
+
+        
+            .then(response => response.json())
+            .then(movie => {
+                
+                this.setState({
+                    movies: [...this.state.movies, movie]
+                })
+            })
+    }
+
+
+    componentDidMount() {
+        this.state.favIds.map((elem) => this.getMovie(elem))
+    }
+
+    render() {
+            
+        return (
             <div>
-                <hi>Favorites</hi>
+                <h1>Favorites</h1>
+                <ul>
+                    {this.state.movies.map((elem) => {
+                        const movieLink = "https://image.tmdb.org/t/p/w300/"
+                        return <li>
+                            <Card
+                                poster_path={movieLink + elem.poster_path}
+                                title={elem.title}
+                                release_date={elem.release_date}
+                                overview={elem.overview}
+                            />
+                        </li>
+                    })}
+                </ul>
+
             </div>
         )
     }
 }
-export default Favorites;
+
+export default Favorites
