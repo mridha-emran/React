29161409473d4e7df968b720937f7a33afa0fe07
mridha-emran <bari.lala490@gmail.com>
2021-11-23 16:21:32 +0100

    new addition

--- chapitre_06/catalog/src/App.js
+++ chapitre_06/catalog/src/App.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from 'react'
+import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './component/Home'
-import Movie from './component/Movie';
+import Movie from './component/Movie'
 
-
-class App extends React.Component{
-  render(){
-    return(
-      <BrowserRouter>
-      
-        <div>
-
-          <h1>Movies</h1>
-
-        </div>
-        <Switch>
-          <Route path = '/' exact component = {Home}></Route> 
-          <Route path = '/:id' exact component = {Movie}></Route>
-
-        </Switch>
-
-      </BrowserRouter>
-    )
-  }
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <h1>Movies</h1>
+                </div>
+                <Switch>
+                    <Route path="/" exact component={Home}></Route>
+                    <Route path="/:id" exact component={Movie}></Route>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
 }
 
-
-export default App;
+export default App
--- chapitre_06/catalog/src/component/Home.js
+++ chapitre_06/catalog/src/component/Home.js
@@ -1,18 +1,20 @@
-import React from "react"
+import React from 'react'
 import { Link } from 'react-router-dom'
 import Movice from '../movice.json'
 
-class Home extends React.Component{
-    render(){
-        return(
+class Home extends React.Component {
+    render() {
+        return (
             <ul>
-                {Movice.map(elem => {
-                 return <li><Link to = {'/' + elem.id}> {elem.title} </Link></li>
-                }
-                )}
+                {Movice.map((elem) => {
+                    return (
+                        <li>
+                            <Link to={'/' + elem.id}> {elem.title} </Link>
+                        </li>
+                    )
+                })}
             </ul>
         )
     }
-    
 }
-export default Home;
+export default Home
